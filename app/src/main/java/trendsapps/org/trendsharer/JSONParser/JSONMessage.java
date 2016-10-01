e96055b8b6bfbbdf6f3685fac6758b7b50d71e7b
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2016-10-02 00:28:17 +0530

    change json object structure

--- app/src/main/java/trendsapps/org/trendsharer/JSONParser/JSONMessage.java
+++ app/src/main/java/trendsapps/org/trendsharer/JSONParser/JSONMessage.java
@@ -1,5 +1,6 @@
 package trendsapps.org.trendsharer.JSONParser;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,33 +15,28 @@ public class JSONMessage {
 //    private int duration;
 //    private String timeStamp;
 
-    private JSONObject header;
-    private JSONObject body;
-    private JSONObject message;
-    private Map<String, Object> headerMap;
-    private Map<String, Object> bodyMap;
-    private Map<String, Object> messageMap;
+    private JSONObject jsonObject;
 
     public JSONMessage(int messageID, String deviceAddress, String shop, String discount,
-                       String content, int duration, String timeStamp) {
-        headerMap = new HashMap<>();
-        bodyMap = new HashMap<>();
-        messageMap = new HashMap<>();
-        headerMap.put("messageId", messageID);
-        headerMap.put("deviceAddress", deviceAddress);
-        bodyMap.put("shop", shop);
-        bodyMap.put("discount", discount);
-        bodyMap.put("content", content);
-        bodyMap.put("duration", duration);
-        bodyMap.put("timeStamp", timeStamp);
-        header = new JSONObject(headerMap);
-        body = new JSONObject(bodyMap);
-        messageMap.put("header", header);
-        messageMap.put("body", body);
-        message = new JSONObject(messageMap);
+                       String content, int duration, String timeStamp) throws JSONException {
+
+        JSONObject header = new JSONObject();
+        header.put("messageID",messageID);
+        header.put("deviceAddress",deviceAddress);
+        JSONObject body = new JSONObject();
+        body.put("shop", shop);
+        body.put("discount", discount);
+        body.put("content", content);
+        body.put("duration", duration);
+        body.put("timeStamp", timeStamp);
+        jsonObject = new JSONObject();
+        jsonObject.put("header", header);
+        jsonObject.put("body", body);
     }
 
     public JSONObject getJSONMessage() {
-        return message;
+        return jsonObject;
     }
+
+
 }
