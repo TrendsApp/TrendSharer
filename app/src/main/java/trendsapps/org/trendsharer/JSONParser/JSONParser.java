004c7d0f8a35504037b96431bc9c3d76cfd25cd7
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2016-10-01 16:39:01 +0530

    JSON Parser Implemented. Not tested

--- app/src/main/java/trendsapps/org/trendsharer/JSONParser/JSONParser.java
+++ app/src/main/java/trendsapps/org/trendsharer/JSONParser/JSONParser.java
@@ -2,44 +2,98 @@ package trendsapps.org.trendsharer.JSONParser;
 
 import org.json.*;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import trendsapps.org.trendsharer.Model.Header;
 import trendsapps.org.trendsharer.Model.Packet;
 import trendsapps.org.trendsharer.Model.HotDeal;
 
-/**
- * Created by jawadhsr on 9/30/16.
- */
+
 public class JSONParser {
 
 
-    public JSONObject ObjecttoJSON(HotDeal object){
+    private JSONObject HotDealtoJSON(HotDeal object){
         JSONObject hotDeal= null;
-        JSONObject fullObject = null;
         try {
-            fullObject = new JSONObject();
             hotDeal = new JSONObject();
             hotDeal.put("shopname",object.getShopName());
             hotDeal.put("content",object.getContent());
             hotDeal.put("discount",object.getDiscount());
             hotDeal.put("date",object.getStoredDate());
 
-            fullObject.put("hotdeal",hotDeal);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return hotDeal;
     }
 
-    public JSONObject fullObjtoJSON(Packet object){
-        JSONObject packet = null;
-
+    public JSONObject PacketToJSON(Packet object){
+        JSONObject packetJSON = null;
+        JSONObject header ;//= null;
+        JSONArray  deals ;//= null;
         try{
-            packet = new JSONObject();
+            packetJSON = new JSONObject();
+            header = new JSONObject();
+            deals = new JSONArray();
 
-            packet.put("id",object.getPacketId());
+            header.put("device_id",object.getHeader().getDeviceID());
+            header.put("device_mac",object.getHeader().getDeviceMAC());
+
+            ArrayList<HotDeal> hotDeals = object.getHotdeals();
+            int i=0;
+            for (HotDeal d : hotDeals) {
+                deals.put(i,this.HotDealtoJSON(d));
+                i++;
+            }
+            packetJSON.put("header",header);
+            packetJSON.put("deals", deals);
+
         }catch (JSONException e){
             e.printStackTrace(); // for the development
         }
 
-        return  packet;
+        return  packetJSON;
+    }
+
+    public Packet JSONtoPacket(JSONObject object){
+        Packet newPacket = null;
+        Header newHeader ;//= null;
+        ArrayList<HotDeal> newDeals ;//= null;
+
+        try{
+            newPacket = new Packet();
+            newHeader = new Header();
+            newDeals = new ArrayList<>();
+            JSONObject header;// = new JSONObject();
+            JSONArray deals ;//= new JSONArray();
+            header = object.getJSONObject("header");
+            deals = object.getJSONArray("deals");
+
+
+            // new header is set
+            newHeader.setDeviceID(header.getInt("device_id"));
+            newHeader.setDeviceMAC(header.getInt("device_mac"));
+            newPacket.setHeader(newHeader);
+
+            // new deals are set
+            for(int i=0;i<deals.length();i++){
+
+                JSONObject aDeal = deals.getJSONObject(i);
+                HotDeal tempDeal = new HotDeal();
+                tempDeal.setShopName(aDeal.getString("shop_name"));
+                tempDeal.setContent(aDeal.getString("content"));
+                tempDeal.setDiscount(aDeal.getString("discount"));
+                tempDeal.setDuration(aDeal.getInt("duration"));
+                tempDeal.setStoredDate((Timestamp) aDeal.get("timestamp"));
+                newDeals.add(i,tempDeal);
+            }
+            newPacket.setHotdeals(newDeals);
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        return newPacket;
     }
 }
---
+++ app/src/main/java/trendsapps/org/trendsharer/Model/Header.java
@@ -0,0 +1,23 @@
+package trendsapps.org.trendsharer.Model;
+
+public class Header {
+
+    private int deviceID;
+    private int deviceMAC;
+
+    public int getDeviceID() {
+        return deviceID;
+    }
+
+    public void setDeviceID(int deviceID) {
+        this.deviceID = deviceID;
+    }
+
+    public int getDeviceMAC() {
+        return deviceMAC;
+    }
+
+    public void setDeviceMAC(int deviceMAC) {
+        this.deviceMAC = deviceMAC;
+    }
+}
