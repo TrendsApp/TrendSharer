c8136084661d5ff09f027b91c0c3b1c8a2857d1d
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-10-03 11:59:22 +0530

    Send button moved to a proper location

--- app/src/main/java/trendsapps/org/trendsharer/MainActivity.java
+++ app/src/main/java/trendsapps/org/trendsharer/MainActivity.java
@@ -437,6 +437,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            sendData(getCurrentFocus());
             return true;
         }
 
